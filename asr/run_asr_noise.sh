6dc19cc8d2f604806fa9cce447a9f1cde381c478
Siddharth Dalmia <sdalmia@andrew.cmu.edu>
2021-04-13 23:16:51 -0400

    bug fix

--- asr/run_asr_noise.sh
+++ asr/run_asr_noise.sh
@@ -4,7 +4,7 @@ nj=1
 text_file=$1
 data_name=$(basename "${text_file}")
 
-#./scripts/run_tts.sh ${text_file}
+./scripts/run_tts.sh ${text_file}
 
 wav_files=espnet/egs/ljspeech/tts1/decode/${data_name}/wav_wnv/
 wav_file=data/${data_name}_wavs
